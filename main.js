c4c90a6dcce3e4dbef35a06244a0fac58cd2f2d8
nikhilpn725 <nikhilpn725@gmail.com>
2023-02-09 22:39:52 +0530

    added now time in hourly forescast in js

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 document.addEventListener("DOMContentLoaded", async () => {
- 
   let cityName = document.getElementById("city-name");
   let mainTemp = document.getElementById("temp-main");
   let descriptionMain = document.getElementById("description-main");
@@ -14,29 +13,40 @@ document.addEventListener("DOMContentLoaded", async () => {
     "inside-hourly-forecast-div-id"
   );
 
-  let feelsLike = document.getElementById("feels-like-h3")
-  let humidity = document.getElementById("humidity-h3")
+  let feelsLike = document.getElementById("feels-like-h3");
+  let humidity = document.getElementById("humidity-h3");
 
   const getHourlyForecast = async () => {
     let response = await fetch(
-      `https://api.openweathermap.org/data/2.5/forecast?q=london&appid=0e1cc6e24661f917b43a7d4c41bbe50b&units=metric&`
+      `https://api.openweathermap.org/data/2.5/forecast?q=kochi&appid=0e1cc6e24661f917b43a7d4c41bbe50b&units=metric&`
     ).then((response) => response.json());
 
     for (let i = 0; i < 10; i++) {
-      let hourlyInnerHtml = `
-      <section>
-            <h4 ">${response.list[i].dt_txt}</h4>
-            <img src=http://openweathermap.org/img/wn/${response.list[i].weather[0].icon}.png>
-            <p>${response.list[i].main.temp}</p>
-          </section>
-      `;
-      insideHourlyForecast.innerHTML += hourlyInnerHtml;
+      if (i === 0) {
+        let hourlyInnerHtml = `
+              <section>
+                    <h4 ">Now</h4>
+                    <img src=http://openweathermap.org/img/wn/${response.list[i].weather[0].icon}.png>
+                    <p>${response.list[i].main.temp}</p>
+                  </section>
+              `;
+        insideHourlyForecast.innerHTML += hourlyInnerHtml;
+      } else {
+        let hourlyInnerHtml = `
+              <section>
+                    <h4 ">${response.list[i].dt_txt}</h4>
+                    <img src=http://openweathermap.org/img/wn/${response.list[i].weather[0].icon}.png>
+                    <p>${response.list[i].main.temp}</p>
+                  </section>
+              `;
+        insideHourlyForecast.innerHTML += hourlyInnerHtml;
+      }
     }
   };
 
   const getWeather = async () => {
     let response = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=london&appid=0e1cc6e24661f917b43a7d4c41bbe50b&units=metric&`
+      `https://api.openweathermap.org/data/2.5/weather?q=kochi&appid=0e1cc6e24661f917b43a7d4c41bbe50b&units=metric&`
     ).then((response) => response.json());
     console.log(response);
 
@@ -53,11 +63,9 @@ document.addEventListener("DOMContentLoaded", async () => {
     high.textContent = `H: ${main.temp_max}`;
     low.textContent = `L: ${main.temp_min}`;
 
-
-    feelsLike.textContent = `${main.feels_like} degrees`
-    humidity.textContent = `${main.humidity}%`
+    feelsLike.textContent = `${main.feels_like} degrees`;
+    humidity.textContent = `${main.humidity}%`;
   };
-
 
   getWeather();
   getHourlyForecast();
